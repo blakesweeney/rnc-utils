da2c595dd20c9456b30661488e4da47875ab7fb4
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-09-16 01:53:21 +0100

    Rework how ids are checked
    The ids we are exporting are actually urs_taxids, so I don't need to do
    all the juggling I was doing before. Also this parts of this, the
    container in particular, could probably be moved to the core crate.

---
+++ active-sequences/src/container.rs
@@ -0,0 +1,35 @@
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+
+use fnv::FnvHashSet;
+use rnc_core::urs::UrsTaxid;
+
+pub struct UrsTaxidContainer {
+    set: FnvHashSet<UrsTaxid>,
+}
+
+impl UrsTaxidContainer {
+    pub fn from_path(path: &Path) -> Result<Self, Box<dyn Error>> {
+        let file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+        let mut set: FnvHashSet<UrsTaxid> = FnvHashSet::default();
+        let mut buf = String::new();
+        loop {
+            match reader.read_line(&mut buf)? {
+                0 => break,
+                _ => {
+                    set.insert(UrsTaxid::try_from(&buf)?);
+                },
+            }
+        }
+        Ok(Self { set })
+    }
+
+    pub fn contains(&self, urs: &UrsTaxid) -> bool {
+        self.set.contains(&urs)
+    }
+}
--- active-sequences/src/main.rs
+++ active-sequences/src/main.rs
@@ -1,6 +1,6 @@
 use std::convert::TryFrom;
-use std::path::PathBuf;
 use std::error::Error;
+use std::path::PathBuf;
 
 extern crate log;
 
@@ -9,11 +9,11 @@ use bio::io::fasta;
 use structopt::StructOpt;
 
 use rnc_core::json_sequence::{each_sequence, Sequence};
-use rnc_core::urs::Urs;
+use rnc_core::urs::UrsTaxid;
 
-pub mod mapping;
+pub mod container;
 
-use crate::mapping::UrsTaxidMapping;
+use crate::container::UrsTaxidContainer;
 
 /// This is a command to process a list of active urs_taxids and urs sequences and produce a
 /// fasta file of the active urs taxids. The sequence file only needs to contain an entry for each
@@ -41,23 +41,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let output = rnc_utils::buf_writer(&opt.output)?;
     let mut writer = fasta::Writer::new(output);
 
-    let urs_mapping = UrsTaxidMapping::from_path(&opt.xref_urs_taxids)?;
+    let container = UrsTaxidContainer::from_path(&opt.xref_urs_taxids)?;
     each_sequence(input, |sequence: Sequence| {
-        let urs = Urs::try_from(sequence.id)?;
-        match urs_mapping.get(&urs) {
-            None => Ok(()),
-            Some(urs_taxids) => {
-                let seq = sequence.sequence.as_bytes();
-                for urs_taxid in urs_taxids {
-                    let record = fasta::Record::with_attrs(
-                        &urs_taxid.to_string(),
-                        sequence.description,
-                        seq
-                    );
-                    writer.write_record(&record)?;
-                }
-                Ok(())
-            }
+        let urs_taxid = UrsTaxid::try_from(sequence.id)?;
+        if container.contains(&urs_taxid) {
+            writer.write_record(&sequence.into())?;
         }
+        Ok(())
     })
 }
