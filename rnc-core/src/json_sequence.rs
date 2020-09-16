44585bd86c5cda0e4005bdb5eb593e86fc31b9c6
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-09-16 02:48:25 +0100

    Deal with description with slashes
    Seems it needs to be a String, not a &str, for some reason.

--- rnc-core/src/json_sequence.rs
+++ rnc-core/src/json_sequence.rs
@@ -1,5 +1,5 @@
-use std::io::prelude::*;
 use std::error::Error;
+use std::io::prelude::*;
 
 use bio::io::fasta;
 
@@ -8,13 +8,17 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Sequence<'a> {
     pub id: &'a str,
-    pub description: Option<&'a str>,
+    pub description: Option<String>,
     pub sequence: &'a str,
 }
 
 impl<'a> From<Sequence<'a>> for fasta::Record {
     fn from(entry: Sequence<'a>) -> fasta::Record {
-        fasta::Record::with_attrs(&entry.id, entry.description, &entry.sequence.as_bytes())
+        fasta::Record::with_attrs(
+            entry.id,
+            entry.description.as_ref().map(|s| s.as_str()),
+            entry.sequence.as_bytes(),
+        )
     }
 }
 
